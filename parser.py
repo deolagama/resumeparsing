e292b535f5e769fffc9ffa4c63534c8c35442454
deolagama <deolagamaa@gmail.com>
2025-07-19 23:18:03 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,15 +1,15 @@
-import fitz  # PyMuPDF
+import fitz  
 import re
-
-def extract_text_from_pdf(file_path):
-    doc = fitz.open(file_path)
-    text = ""
+def extract_text_from_pdf(file_path): 
+    doc = fitz.open(file_path)  
+    text = "" 
     for page in doc:
         text += page.get_text()
     return text
 
+
 def extract_email(text):
-    match = re.search(r'[\w\.-]+@[\w\.-]+', text)
+    match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', text)
     return match.group(0) if match else "Not found"
 
 def extract_phone(text):
@@ -18,8 +18,8 @@ def extract_phone(text):
 
 def extract_skills(text):
     skill_keywords = ['python', 'java', 'sql', 'html', 'css', 'flask', 'c++', 'react']
-    found = [skill for skill in skill_keywords if skill.lower() in text.lower()]
-    return found
+    found = [skill for skill in skill_keywords if skill.lower() in text.lower()] 
+    return found #The item you’re adding to the final list- is the first skill
 
 def extract_name(text):
     lines = text.strip().split('\n')
@@ -32,5 +32,5 @@ def extract_education(text):
     edu_keywords = ['bachelor', 'master', 'b.tech', 'm.tech', 'b.sc', 'm.sc']
     for line in text.lower().split('\n'):
         if any(word in line for word in edu_keywords):
-            return line.strip()
+            return line.strip() 
     return "Not found"
